c47c17d1c55896c151e588b7c40dd8da932063f7
Ogawa-Nobita <d-ogawa@origina-inc.jp>
2026-08-13 08:09:41 +0900

    名札を頭上から机の前面へ移動し顔が隠れないようにする

--- viewer/src/scene/Office.tsx
+++ viewer/src/scene/Office.tsx
@@ -3,15 +3,16 @@ import { ACTORS, ACTOR_COLORS, ACTOR_LABELS, DESK_POSITIONS, PALETTE } from "../
 import { Desk } from "./Desk";
 import { Avatar } from "./Avatar";
 
+// 顔にかぶらないよう、頭上ではなく机の手前側(客側の縁)に立てる名札。
 // 机ごとに向きが異なるため、常にカメラの方を向くBillboardにして文字が裏返らないようにする
 function NameSign({ actor }: { actor: (typeof ACTORS)[number] }) {
   return (
-    <Billboard position={[0, 2.05, 0]}>
+    <Billboard position={[0, 0.98, -0.58]}>
       <mesh>
-        <planeGeometry args={[1.3, 0.5]} />
+        <planeGeometry args={[0.9, 0.32]} />
         <meshStandardMaterial color={PALETTE.wallShoji} roughness={1} metalness={0} />
       </mesh>
-      <Text position={[0, 0, 0.01]} fontSize={0.26} color={PALETTE.ink} anchorX="center" anchorY="middle">
+      <Text position={[0, 0, 0.01]} fontSize={0.19} color={PALETTE.ink} anchorX="center" anchorY="middle">
         {ACTOR_LABELS[actor]}
       </Text>
     </Billboard>
